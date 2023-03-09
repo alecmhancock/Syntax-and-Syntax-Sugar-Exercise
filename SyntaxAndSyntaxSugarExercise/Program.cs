e88eb4687aec6b073d00b2a8e67a0da09e1a52c9
Alec Hancock <alec.m.hancock@gmail.com>
2023-03-09 11:30:32 -0600

    Assignment complete

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Given the following code, change the code to implement:
+            //inferred typing
+            //string interpolation
+            //and the ternary operator
+
+
+            // define variables as var
+
+            var answer = 4;
+            var response = (answer < 9) ?  $"{answer} is less than nine." : $"{answer} is greater than or equal to nine.";
+
+            Console.WriteLine($"{response}");
+
+
+
+
+
         }
     }
 }
